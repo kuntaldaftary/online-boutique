5c9ee16818862ed366ca7e433eaf8b0d84a9c490
Kuntal Daftary <kuntaldaftary@gmail.com>
2025-05-02 13:12:45 -0700

    force gloud cluster get-credentials

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,11 +65,15 @@ module "gcloud" {
 
   platform              = "linux"
   additional_components = ["kubectl", "beta"]
+  enabled               = true  # REQUIRED
 
   create_cmd_entrypoint = "gcloud"
   # Module does not support explicit dependency
   # Enforce implicit dependency through use of local variable
   create_cmd_body = "container clusters get-credentials ${local.cluster_name} --zone=${var.region} --project=${var.gcp_project_id}"
+  create_cmd_triggers = {
+    always_run = timestamp() # ensures it runs every time
+  }
 }
 
 # Apply YAML kubernetes-manifest configurations
